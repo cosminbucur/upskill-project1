85fad8abd81b1981389c5ea490921495044a690d
AZglavoci <anatolie.zglavoci@gmail.com>
2021-04-14 17:20:24 +0300

    up4 Save location WIP

--- src/main/java/com/sda/weather/model/Location.java
+++ src/main/java/com/sda/weather/model/Location.java
@@ -1,4 +1,85 @@
 package com.sda.weather.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "location")
 public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "location_name")
+    private String locationName;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    private String region;
+
+    @Column(name = "country_name")
+    private String countryName;
+
+    public Location() {
+    }
+
+    public Location(String locationName, Double latitude, Double longitude, String region, String countryName) {
+        this.locationName = locationName;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.region = region;
+        this.countryName = countryName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+
 }
